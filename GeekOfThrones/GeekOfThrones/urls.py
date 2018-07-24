3eb38690cdd006ba5c8df9734a484ea6612f866b
temidayoadefioye <temidjoy@hotmail.com>
2018-07-24 07:42:06 +0100

    got v1

--- GeekOfThrones/GeekOfThrones/urls.py
+++ GeekOfThrones/GeekOfThrones/urls.py
@@ -18,7 +18,7 @@ from django.conf.urls import url
 from django.contrib import admin
 from django.conf.urls.static import static
 
-from GOTWebsite import settings
+from GeekOfThrones import settings
 from app import views
 
 urlpatterns = [
